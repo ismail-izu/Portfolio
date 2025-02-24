f71b5dabae496c72adf7c66a646858491c513a16
ismail <ismailizu789@gmail.com>
2025-02-24 23:14:26 +0530

    PF-2 - Implemented initial routing to home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet]
 })
 export class AppComponent {
-  title = 'Portfolio';
+  constructor(private router: Router) {
+    this.navigateToHome();
+  }
+  navigateToHome() {
+    this.router.navigate(['/home']);
+  }
+
 }
