01794696b2ddb428bf731ab17eb2f4dd16d7ed1e
ismail <ismailizu789@gmail.com>
2025-02-25 22:48:09 +0530

    PF-3 - Added Footer component to the app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { Router, RouterModule, RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [NavBarComponent,RouterOutlet]
+  imports: [NavBarComponent,RouterOutlet,FooterComponent]
 })
 export class AppComponent {
   constructor(private router: Router) {
