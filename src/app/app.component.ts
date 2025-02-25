183fdaeb3247d87538b23ad3e26d17f4620a9205
ismail <ismailizu789@gmail.com>
2025-02-25 21:30:20 +0530

    PF-3 - Used Navbar component for routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [RouterOutlet]
+  imports: [NavBarComponent,RouterOutlet]
 })
 export class AppComponent {
   constructor(private router: Router) {
