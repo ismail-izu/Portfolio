458da7f1378cf58e1e2a2c6f56b6e675d9f609d1
ismail <ismailizu789@gmail.com>
2025-03-09 14:38:22 +0530

    PF-13 - Remove commented codes in app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,3 @@
 import { Routes } from '@angular/router';
-// import { HomeComponent } from './home/home.component';
-// import { AppComponent } from './app.component';
-// import { SkillsComponent } from './skills/skills.component';
-// import { AboutComponent } from './about/about.component';
-// import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [
-    // { path: '', component: AppComponent },
-    // { path: 'home', component: HomeComponent },
-    // { path: 'skills', component: SkillsComponent },
-    // { path: 'about', component: AboutComponent },
-    // { path: 'contact', component: ContactComponent },
-];
+export const routes: Routes = [];
