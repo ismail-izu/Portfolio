1ae092e291d8909f9300f878409ffcb1dfd6e90c
ismail <ismailizu789@gmail.com>
2025-03-09 14:05:25 +0530

    PF-12 - Responsive navbar component

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
-
+  isMenuOpen: boolean = false;
 }
